4a8898f609672e3c2636b97361165e8d9a13f329
WaitHoldMyBeer <kenanblair2@gmail.com>
2022-06-07 20:24:46 -0700

    Update U2014Decpaint.java

--- general/src/U2014Decpaint.java
+++ general/src/U2014Decpaint.java
@@ -9,12 +9,33 @@ public class U2014Decpaint {
 		
 	}
 	public static void init() throws IOException{
-		in = new Scanner(new File());
+		in = new Scanner(new File("paint.in"));
+		out = new PrintWriter("paint.out");
+		
+		a = in.nextInt();
+		b = in.nextInt();
+		c = in.nextInt();
+		d = in.nextInt();
+		
+		output = -1;
 	}
 	public static void solve() {
-		
+		int[] fences = new int[101];
+		for (int i = a; i < b; i++) {
+			fences[i] = 1;
+		}
+		for (int i = c; i < d; i++) {
+			fences[i] = 1;
+		}
+		int sum = 0;
+		for (int i = 0; i < 101; i++) {
+			sum+= fences[i];
+		}
+		output = sum;
 	}
 	public static void output() {
-		
+		out.println(output);
+		out.close();
+		in.close();
 	}
 }
