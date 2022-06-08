c9ac9513fb734ab04463150e709639a6590e96b7
WaitHoldMyBeer <kenanblair2@gmail.com>
2022-06-07 20:24:22 -0700

    finished usaco bronze

---
+++ general/src/U2014Decpaint.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+public class U2014Decpaint {
+	static Scanner in;
+	static PrintWriter out;
+	static int output;
+	static int a,b,c,d;
+	public static void main(String[] args) throws IOException{
+		
+	}
+	public static void init() throws IOException{
+		in = new Scanner(new File());
+	}
+	public static void solve() {
+		
+	}
+	public static void output() {
+		
+	}
+}
